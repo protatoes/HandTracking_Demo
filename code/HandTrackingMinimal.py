8c48f1539b1dfc78541a0da330e0452d74374cab
Shawn Mishra <44509537+protatoes@users.noreply.github.com>
2023-02-23 21:25:55 -0500

    updated app.py
    Updated app.py for to display images using PyQt6

--- code/HandTrackingMinimal.py
+++ code/HandTrackingMinimal.py
@@ -1,53 +1,68 @@
 import time
 import numpy as np
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import cv2
 import mediapipe as mp
+import pandas as pd
 
 #minimum working example
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 #Define mediapipe tracking instance, and drawing utility
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode = False,
                                         max_num_hands = 2,
-                                        min_detection_confidence = 0.3,
-                                        min_tracking_confidence = 0.3, #tracking confidence will allow for the same hand to be tracked throughout
+                                        min_detection_confidence = 0.4,
+                                        min_tracking_confidence = 0.4, #tracking confidence will allow for the same hand to be tracked throughout
                                         model_complexity = 1)
 mpDraw = mp.solutions.drawing_utils
 
 
 #setup calc for frame_rate calculation
 # 1 / (current time - previous time)
+startTime = time.time()
 pTime = 0   
 cTime = 0
-
+hand_dict_res =  pd.DataFrame(columns=['handIndex',"hand_label",'timestamp','finger', 'x', 'y','z'])
 #Start live stream from webcam
 while True:
     success, img = cap.read()#get the frame
+    #Try to improve performance 
+    img.flags.writeable = False
     #convert image to RGB, mediapipe only takes rgb images
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #get detection,tracking results 
     results = hands.process(imgRGB)
-
+    img.flags.writeable = True
      #check if a hand is detected
     if results.multi_hand_landmarks:
         #iterate over each hand and extract landmark information
         for handLms in results.multi_hand_landmarks:
-            #get hand index and finger landmarks per hand
+            #get the hand_index and label
+            handIndex = results.multi_hand_landmarks.index(handLms)
+            hand_label = results.multi_handedness[handIndex].classification[0].label
+            #get finger index and finger landmarks per hand
             for id,lm in enumerate(handLms.landmark):
-                
                 #get image size to get the center of the landmarks in pixels
                 h,w,c = img.shape
-                cx,cy = int(lm.x*w),int(lm.y*h) 
+                cx,cy,cz = int(lm.x*w),int(lm.y*h),int(lm.z*w) 
                 #get a particular landmark 
                 if id == 8 or id == 12:
                     cv2.circle(img, (cx,cy),15,(255,0,0),cv2.FILLED)
-                    print(time.time(),id, cx,cy)
-
+                    row_dict = {"handIndex":handIndex,
+                        "hand_label" : hand_label,
+                        "timestamp": time.time() - startTime,
+                        "finger": id,
+                        'x': cx,
+                        'y':cy,
+                        'z':cz}
+                    hand_dict_res = hand_dict_res.append(row_dict, ignore_index=True)
             #drawn the hand landmarks and connections onto the img
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS) 
-
+            
     #frame rate calc
     cTime = time.time()
     fps = 1/(cTime - pTime)
--- code/app.py
+++ code/app.py
@@ -1,46 +1,90 @@
-import time
-import threading
+import sys
 
-import numpy as np
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
-from matplotlib import pyplot as plt
+import cv2
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
 
 import mediapipe as mp
-from PIL import Image
-import cv2
-from HandModule import handDetector
-
-#Set some header text
-st.title("Digital Biomarkers Orthogonal Validaiton Demo")
-st.write("Some text here")
 
 
-lock = threading.Lock()
-img_container = {"img": None}
+# Subclass QMainWindow to customize your application's main window 
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        #set the layout for the window
+        self.VBL = QVBoxLayout()
+        #Populate the layout with widgets
+        #Will be used to display image from webacm
+        self.FeedLabel = QLabel()
+        self.VBL.addWidget(self.FeedLabel)
 
 
-#sample video call back that 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
+        #Create instance of QThread class
+        self.Worker1 = Worker1()
+        #start the function and emit a signal
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
 
-    return frame
+        #Cancel Button to stop videofeed
+        self.CancelBTN = QPushButton('Cancel')
+        self.CancelBTN.clicked.connect(self.CancelFeed)
+        self.VBL.addWidget(self.CancelBTN)
 
 
-ctx = webrtc_streamer(key="example",video_frame_callback=video_frame_callback)
+        self.setLayout(self.VBL)
 
-fig_place = st.empty()
-fig, ax = plt.subplots(1, 1)
 
-while ctx.state.playing:
-    with lock:
-        img = img_container["img"]
-    if img is None:
-        continue
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ax.cla()
-    ax.hist(gray.ravel(), 256, [0, 256])
-    fig_place.pyplot(fig)
+    def ImageUpdateSlot(self,Image):
+        #capture the emitted image from worker1 and update the FeedLabel
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+
+    def CancelFeed(self):
+        self.Worker1.stop()
+
+class Worker1(QThread):
+    #handle retrieving image from webcam
+    ImageUpdate = pyqtSignal(QImage)
+
+    #will be called when we call the .start argument for the QThread object
+    def run(self):
+        self.ThreadActive = True
+        capture = cv2.VideoCapture(0)
+        
+        #This is the where we will run  the video and I guess do the processing
+        #similar to the while loop in the TestBed notebook
+        while self.ThreadActive:
+            ret, frame = capture.read()
+            if ret: 
+                #convert to rgb format
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImg = cv2.flip(img, 1)
+                #Convert to PIC image that PyQT can deal with
+                ConvertToQtFormat = QImage(FlippedImg.data,FlippedImg.shape[1],FlippedImg.shape[0], QImage.Format.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(640,480, Qt.AspectRatioMode.KeepAspectRatio)
+                #this will send a signal to the MainWindow of the app, 
+                # transmitting the data under Pic
+                self.ImageUpdate.emit(Pic)
+
+        capture.release()
+
+
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
+
+
+
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
